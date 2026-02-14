b52708a4e13699f0242de167dc572ec4c4ed377f
EdwardHewes <edwardhewes.writing@outlook.com>
2026-02-14 14:51:44 +0900

    Use uploaded my-photo as homepage avatar

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,4 +1,4 @@
-import avatar from '../assets/images/avatar.jpg';
+import avatar from '../assets/images/my-photo.jpeg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
